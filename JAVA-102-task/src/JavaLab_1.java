57a7319ab497ca64ef1a51d60b1622e116fd1869
Jun <jun55tsuno@gmail.com>
2022-10-05 13:57:00 -0700

    finished tasks

--- JAVA-102-task/src/JavaLab_1.java
+++ JAVA-102-task/src/JavaLab_1.java
@@ -24,44 +24,42 @@ public class JavaLab_1 {
 
 // ## 2 — Convert Cents to Dollars-----------------------------
 
-        Scanner scan2;
-        int inputNum2;
-        int dollers;
-        int cents;
+    Scanner scan2;
+    int inputNum2;
+    int dollers;
+    int cents;
 
-        scan2 = new Scanner(System.in);
+    scan2 = new Scanner(System.in);
 
-        System.out.print("Input the cents: ");
-        inputNum2 = scan2.nextInt();
-        dollers = inputNum2 / 100;
-        cents = inputNum2 % 100;
+    System.out.print("Input the cents: ");
+    inputNum2 = scan2.nextInt();
+    dollers = inputNum2 / 100;
+    cents = inputNum2 % 100;
 
-        System.out.println("The value is " + dollers + " dollars " + "and " + cents + " cents.");
+    System.out.println("The value is " + dollers + " dollars " + "and " + cents + " cents.");
 
 
 // ## 3 - Multiplication Table-----------------------------
 
-        Scanner scan3;
-        int inputNum3;
+    Scanner scan3;
+    int inputNum3;
 
-        scan3 = new Scanner(System.in);
+    scan3 = new Scanner(System.in);
 
-        System.out.print("Input a number: ");
-        inputNum3 = scan3.nextInt();
+    System.out.print("Input a number: ");
+    inputNum3 = scan3.nextInt();
 
-        for(int i = 1; i <= 10; i++) {
-            System.out.println(inputNum3 + " x " + i + " = " + inputNum3 * i);
-        }
+    for(int i = 1; i <= 10; i++) {
+        System.out.println(inputNum3 + " x " + i + " = " + inputNum3 * i);
+    }
 
 
 // ## 4 - Print the Pattern-----------------------------
 
-            // System.out.printf("%S");
-
-            System.out.printf("%1$4s %2$6s %3$9s %4$3s %n",  "J", "a", "v     v", "a");
-            System.out.printf("%1$4s %2$7s %3$7s %4$5s %n" , "J", "a a", "v   v" , "a a");
-            System.out.printf("%1$4s %2$8s %3$5s %4$7s %n" , "J  J", "aaaaa", "V V" , "aaaaa");
-            System.out.printf("%1$3s %2$10s %3$3s %4$9s %n" , "JJ", "a     a", "V" , "a     a");
+    System.out.printf("%1$4s %2$6s %3$9s %4$3s %n",  "J", "a", "v     v", "a");
+    System.out.printf("%1$4s %2$7s %3$7s %4$5s %n" , "J", "a a", "v   v" , "a a");
+    System.out.printf("%1$4s %2$8s %3$5s %4$7s %n" , "J  J", "aaaaa", "V V" , "aaaaa");
+    System.out.printf("%1$3s %2$10s %3$3s %4$9s %n" , "JJ", "a     a", "V" , "a     a");
 
     }
 }
